f86f26466793c8163715370e49d24beaed06b5fc
Mrunal Jagtap <mrunaljagtap395@gmail.com>
2026-08-04 00:54:15 +0530

    just small change

--- Backend/src/lib/Socket.js
+++ Backend/src/lib/Socket.js
@@ -1,21 +1,32 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import express from "express"
-const app=express();
-const server=http.createServer(app);
+import cors from"cors";
 
-const io=new Server(server,{
-    Cors:{
-        origin:["http:localhost:5173"],
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST"],
+        credentials: true,
     },
-})
+});
 
-io.on("connection",(socket)=>{
-    console.log("A User Connected ",socket.id);
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
-    socket.on("disconnect",()=>{
-        console.log("A User disconneted",socket.id)
+io.on("connection", (socket) => {
+    console.log("A User Connected ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("A User disconneted", socket.id)
     })
 })
 
-export {io,app,server};
+export { io, app, server };
