68b8e2f1d65a03088b7e55e2b3773e31c8a268d3
Mrunal Jagtap <mrunaljagtap395@gmail.com>
2026-08-04 23:58:23 +0530

    Fix production API base URL

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8080" : "";
+
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:8080/api",
+    baseURL: `${BASE_URL}/api`,
     withCredentials:true,  //sending cookies to the backend server with api request
 })
